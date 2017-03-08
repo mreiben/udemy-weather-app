511aed43a8adfb4e02c5a2f4d16e858e2463bdca
Jason Eiben <jason.eiben@gmail.com>
2017-03-07 21:21:55 -0500

    style weather search

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,7 +4,7 @@ var WeatherMessage = (props) => {
   var {temp, location} = props;
 
   return (
-    <h3>It is {temp} in {location}</h3>
+    <h3 className="text-center">It is {temp} in {location}</h3>
   );
 }
 
